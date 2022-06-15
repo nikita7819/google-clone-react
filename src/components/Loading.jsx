ce6e7e9a3c4d945da526f27a2e081a69da23e415
Nikita <nikitakubavat33@gmail.com>
2022-06-15 20:13:32 +0530

    done

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BallTriangle, Circles, Hearts, Oval, Rings, TailSpin, ThreeDots} from 'react-loader-spinner';
+import {TailSpin} from 'react-loader-spinner';
 
 
 const Loading = () => {
